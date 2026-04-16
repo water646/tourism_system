823b102cd3b20ddfdde88493eccf1400c0108195
water646 <43556262@qq.com>
2026-04-16 11:13:45 +0800

    4.16改不用动前端了

--- src/utils/https.js
+++ src/utils/https.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const http = axios.create({
-  baseURL: 'http://wfda8599.natappfree.cc',
+  baseURL: 'http://cd65f6a4.natappfree.cc',
   timeout: 5000,
 })
 export default http
